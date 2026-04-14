36e10a5f37f87ea6e8e9ad7783f9b2da4353f4c9
Kashyap <Kashyap.k@empiricinfotech.com>
2026-04-14 11:37:10 +0530

    updated url in frontend

--- backend/server.js
+++ backend/server.js
@@ -20,5 +20,5 @@ app.use("/api/", authRoutes)
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-    console.log(`Server running on port http://localhost:${port}`);
+    console.log(`Server running on port ${port}`);
 });
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_APP_URL,
+  baseURL: `${import.meta.env.VITE_APP_URL}/api`,
   withCredentials: true,
 });
 
